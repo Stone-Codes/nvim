ca98c6d07a1e4c058814360b67fb8409fd1c23b0
Stone-Codes <mail@dietz.codes>
2023-12-24 22:32:21 +0100

    Add go stuff

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -6,7 +6,7 @@ require('mason').setup({})
   require('mason-lspconfig').setup({
     -- Replace the language servers listed here 
     -- with the ones you want to install
-    ensure_installed = {'tsserver',  'lua_ls', 'eslint', 'svelte'},
+    ensure_installed = {'tsserver',  'lua_ls', 'eslint', 'svelte', 'tailwindcss', 'gopls', 'templ'},
     handlers = {
       lsp.default_setup,
     },

65c075ddb345919b29e8a3840c74eb3fb4d17136
Stone-Codes <mail@dietz.codes>
2024-01-03 16:04:11 +0100

    Add black

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -6,7 +6,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
-  ensure_installed = { 'tsserver', 'lua_ls', 'eslint', 'svelte', 'tailwindcss', 'gopls', 'templ' },
+  ensure_installed = { 'tsserver', 'lua_ls', 'eslint', 'svelte', 'tailwindcss', 'gopls', 'templ', 'black' },
   handlers = {
     lsp.default_setup,
   },
